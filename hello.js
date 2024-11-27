abf5f76610eb4b915f9969cdb1e46757e18fdc11
saiteja-2005 <147533573+saiteja-2005@users.noreply.github.com>
2024-11-27 14:13:58 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,6 @@
 const http = require('http');
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-    // Set the response header
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     if (req.url === '/') {
         res.end('Welcome to the Home Page!');
     } else {
